b551cc400966d172474f13f09289dc1f401e0263
huangsiyu-home <aiolosgs@163.com>
2019-11-19 23:42:20 +0800

    created by vscode

---
+++ clienta-demo/src/main/java/com/aiolosgs/clientademo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.aiolosgs.clientademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
