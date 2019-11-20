f627d927453702553afff9bc63e3bb3437c4f5bd
huangsiyu <aiolosgs@163.com>
2019-11-20 21:42:24 +0800

    集成eureka

--- clienta-demo/src/main/java/com/aiolosgs/clientademo/DemoApplication.java
+++ clienta-demo/src/main/java/com/aiolosgs/clientademo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.aiolosgs.clientademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient//eureka客户端
 @SpringBootApplication
 public class DemoApplication {
 
--- springboot-demo/src/main/java/com/bootdemo/ApplicationStarter.java
+++ springboot-demo/src/main/java/com/bootdemo/ApplicationStarter.java
@@ -2,7 +2,9 @@ package com.bootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer//eureka注册中心
 @SpringBootApplication
 public class ApplicationStarter {
 
