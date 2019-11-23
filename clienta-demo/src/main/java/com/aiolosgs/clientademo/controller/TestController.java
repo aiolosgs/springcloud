4285202b2e7561104d5425aaf26eeda68c388374
huangsiyu-home <aiolosgs@163.com>
2019-11-23 13:20:46 +0800

    配置中心

--- clienta-demo/src/main/java/com/aiolosgs/clientademo/controller/TestController.java
+++ clienta-demo/src/main/java/com/aiolosgs/clientademo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.aiolosgs.clientademo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +15,24 @@ public class TestController {
 
 	@Autowired
 	private DemoBAPI clientB;
+
+	@Value("${server.port}")
+	private String port;
+
+	@Value("${config.test}")
+	private String config;
 	
 	@RequestMapping(value="get")
 	public DemoA get(){
 		DemoA a = new DemoA();
 		a.setId("1");
-		a.setName("entity from client-a");
+		a.setName("entity from client-a via port : " + port);
 		return a;
+	}
+
+	@RequestMapping(value="getConfig")
+	public String getConfig(){
+		return config;
 	}
 	
 	@RequestMapping(value="getB")
--- clientb-demo/src/main/java/com/aiolosgs/clientbdemo/controller/BaseController.java
+++ clientb-demo/src/main/java/com/aiolosgs/clientbdemo/controller/BaseController.java
@@ -1,6 +1,8 @@
 package com.aiolosgs.clientbdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,18 +12,30 @@ import com.aiolosgs.model.DemoB;
 
 @RestController
 @RequestMapping(value="base")
+@RefreshScope
 public class BaseController {
 
 	@Autowired
 	private DemoAAPI clientA;
+
+	@Value("${server.port}")
+	private String port;
+
+	@Value("${config.test}")
+	private String config;
 	
 	@RequestMapping(value="get")
 	public DemoB get(){
 		DemoB b = new DemoB();
 		b.setId("1");
-		b.setName("entity from client-b");
+		b.setName("entity from client-b via port : " + port);
 		b.setCode("123");
 		return b;
+	}
+
+	@RequestMapping(value="getConfig")
+	public String getConfig(){
+		return config;
 	}
 	
 	@RequestMapping(value="getA")
